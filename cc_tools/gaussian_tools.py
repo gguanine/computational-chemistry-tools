de8b6bb1b186e2f81530e998042e79f2409d2899
gg <gg@gg.g>
2025-03-18 13:47:59 +0800

    bug fix

--- cc_tools/gaussian_tools.py
+++ cc_tools/gaussian_tools.py
@@ -1,98 +1,40 @@
 from .config import HPC_NPROCS, HPC_MEM, GAUSSIAN_CHECK
 from . import utils
 
-import periodictable
 from rdkit import Chem
 from rdkit.Chem import AllChem
-import cclib
 
 import os
   
 
-def molecule_from_smiles(smiles:str, name=None, charge=None, multiplicity=None) -> Chem.Mol:
-    mol = Chem.MolFromSmiles(smiles)
-    mol = Chem.AddHs(mol)
-    AllChem.EmbedMolecule(mol, randomSeed=1)
-    AllChem.MMFFOptimizeMolecule(mol)
-
-    utils.set_cc_props(mol, name, charge, multiplicity)
-    utils.print_mol_data(mol)
-    
-    return mol
-
-def molecule_from_log(filename:str, name_ending:str="-out", allow_imag:bool = False) -> Chem.Mol:
-    # When allow_imag is False, imaginary frequency is not allowed
-
-    name = os.path.splitext(os.path.basename(filename))[0] + "_" + name_ending
-    print(f"\n\nNow reading molecule {name} in {filename}")
-
-    data = cclib.io.ccopen(filename).parse()
-
-    # Check if the Gaussian terminate nomally
-    if not data.metadata["success"]:
-        print("Error termination! Gaussian did not end normally.")
-        return None
-
-    # Check imaginary frequency
-    try:
-        if data.vibfreqs[0] < 0: # freq calculation may not perform
-            print("Imaginary frequency is presented")
-            if not allow_imag:
-                print("***********Imaginary frequency is not allowed for this computation (by default).***********")
-                return None
-    except:
-        pass
-
-
-    XYZBlock = str(data.natom) + "\n\n"
-    atomlabel = [periodictable.elements[i].symbol for i in data.atomnos]
-    coords = ["   ".join(map("{:.7f}".format, row)) for row in data.atomcoords[-1]]
-    atomNcoords = ["   ".join(row) for row in zip(atomlabel, coords)]
-    cartesian = "\n".join(atomNcoords)
-    XYZBlock += cartesian
-
-    mol = Chem.MolFromXYZBlock(XYZBlock)
-    mol.SetProp("Name", name)
-    mol.SetProp("Charge", str(data.charge))
-    mol.SetProp("Multiplicity", str(data.mult))
-    mol.SetProp("Cartesian", cartesian) 
-    #print_mol_data(mol)
-    print("Successfully load the molecule.")
-
-    return mol
-
-def molecule_from_gjf(filename:str, output_ending_with:str="-out", MM_opt=False) -> Chem.Mol:
+def molecule_from_gjf(filename:str, name_suffix:str="_2") -> Chem.Mol:
     # a parser to read molecule from gaussian input file
     with open(filename, 'r') as f:
         text = f.read().strip()
-        sections = text.split("\n\n")
         
-        mol_name = sections[1].strip()
-        print("Now reading", mol_name)
+    sections = text.split("\n\n")
         
-        section2_lines = sections[2].splitlines()
-        charge_multiplicity = section2_lines[0].split()
-        charge = int(charge_multiplicity[0])
-        multiplicity = int(charge_multiplicity[1])
-        
-        natom = len(section2_lines[1:])
-        cartesian = "\n".join(section2_lines[1:])
-        XYZBlock = str(natom) + '\n\n' + cartesian
+    section2_lines = sections[2].splitlines()
+    charge, multiplicity = map(int, section2_lines[0].split())  # the first line of section 2 in charge and mult
+    
+    natom = len(section2_lines[1:])  # the rest of section 2 is cartesian
+    cartesian = "\n".join(section2_lines[1:])
+    XYZBlock = str(natom) + '\n\n' + cartesian
 
-        mol = Chem.MolFromXYZBlock(XYZBlock)
+    # embed molecule
+    mol = Chem.MolFromXYZBlock(XYZBlock)
+    
+    name = sections[1].strip() # molecule name is always in the second section of .gjf file
+    if name == '':
+        print("Molecule name is empty. Setting the name as InChI Key")
+        name = Chem.inchi.MolToInchiKey(mol) 
+    print("Now reading", name)
         
-        if MM_opt is True:
-            AllChem.MMFFOptimizeMolecule(mol)
-            
-        mol.SetProp("Name", mol_name)
-        mol.SetProp("Charge", str(charge))
-        mol.SetProp("Multiplicity", str(multiplicity))
-        mol.SetProp("Cartesian", cartesian) 
-        
-        #print_mol_data(mol)
-        print("Successfully load the molecule.")           
-        
-        return mol
+    utils.set_cc_props(mol, name, charge, multiplicity)
+    utils.print_mol_data(mol)
+    print("Successfully load the molecule.")           
+    
+    return mol
 
 class GaussianInputFile():
     def __init__(self, file_directory:str=None, nprocs:int=HPC_NPROCS, mem:str=HPC_MEM, check:bool=GAUSSIAN_CHECK):
@@ -135,21 +77,18 @@ class GaussianInputFile():
             f.write("\n" + cmd2 + "\n\n")
         print("done!\n" + "-"*100 + "\n")
 
-def dir_logs_to_gjf(input_dir:str, output_dir:str, cmd:str, name_ending=None):
+def dir_logs_to_gjf(input_dir:str, output_dir:str, cmd:str, name_suffix):
     # finding  Gaussian output .log files in input direactory, convert them into gjf using command cmd.
-    if name_ending is None:
-        name_ending = input_dir
     
-    cwd = os.getcwd()
     log_files = []
-    for root, dirs, files in os.walk(os.path.join(cwd, input_dir)):
+    for root, dirs, files in os.walk(input_dir):
         for file in files:
             if file.endswith('.log'):
                 full_file_dir = os.path.join(root, file)
                 print("Found:", full_file_dir)
                 log_files.append(full_file_dir)
                 
-    mols = [molecule_from_log(f, name_ending=name_ending) for f in log_files]            
+    mols = [utils.molecule_from_log(f, name_suffix=name_suffix) for f in log_files]            
                 
     g_en = GaussianInputFile(file_directory=output_dir)
     for mol in mols:
@@ -158,9 +97,8 @@ def dir_logs_to_gjf(input_dir:str, output_dir:str, cmd:str, name_ending=None):
 def dir_gjfs_to_gjf(input_dir:str, output_dir:str, cmd:str, MM_opt=False):
     # finding  Gaussian input .gjf files in input direactory, convert them into gjf using command cmd.
     
-    cwd = os.getcwd()
     original_files = []
-    for root, dirs, files in os.walk(os.path.join(cwd, input_dir)):
+    for root, dirs, files in os.walk(input_dir):
         for file in files:
             if file.endswith('.gjf'):
                 full_file_dir = os.path.join(root, file)
--- cc_tools/utils.py
+++ cc_tools/utils.py
@@ -1,5 +1,10 @@
 from rdkit import Chem
+from rdkit.Chem import AllChem
 from rdkit.Chem import Descriptors
+import cclib
+import periodictable
+
+import os
 
 def xyz_block_to_cartesian(XYZBlock:str) -> str:
     # delete the first two lines of XYZ block to give Cartesian.
@@ -7,13 +12,17 @@ def xyz_block_to_cartesian(XYZBlock:str) -> str:
 
 def get_cartesian(mol:Chem.Mol) -> str:
     return xyz_block_to_cartesian(Chem.MolToXYZBlock(mol))
-
         
 def set_cc_props(mol:Chem.Mol, name:str=None, charge:int=None, multiplicity:int=None):
+    '''
+    Set necessary molecular properties for computation chemistry: name, charge, and multiplicity\n
+    Default properties will be calculated from molecule if not provided.
+    '''
+    
     if name is None:
         print("Molecule name is empty. Setting the name as InChI Key")
         name = Chem.inchi.MolToInchiKey(mol)  
-    mol.SetProp("_Name", name)
+    mol.SetProp("_Name", name) # Using _Name instead of Name to improve RDKit compatibility
     
     if charge is None:
         charge = Chem.GetFormalCharge(mol)
@@ -27,12 +36,65 @@ def set_cc_props(mol:Chem.Mol, name:str=None, charge:int=None, multiplicity:int=
     mol.SetProp("Multiplicity", str(multiplicity)) 
     
 def print_mol_data(mol:Chem.Mol):
-    # print molecule information
+    # print molecule properties
     print("name =", mol.GetProp("_Name"))
     print("charge =", mol.GetProp("Charge"))
     print("multiplicity =", mol.GetProp("Multiplicity"))
-    print(get_cartesian(mol)) 
+    print(get_cartesian(mol), "\n\n") 
         
+def molecule_from_smiles(smiles:str, name=None, charge=None, multiplicity=None) -> Chem.Mol:
+    mol = Chem.MolFromSmiles(smiles)
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(mol, randomSeed=6)
+
+    set_cc_props(mol, name, charge, multiplicity)
+    print_mol_data(mol)
+    
+    return mol
+
+def molecule_from_log(filename:str, name_suffix:str="out", allow_imag:bool = False) -> Chem.Mol:
+    # When allow_imag is False, imaginary frequency is not allowed
+
+    name = os.path.splitext(os.path.basename(filename))[0] + "_" + name_suffix
+    print(f"\n\nNow reading molecule {name} in {filename}")
+
+    data = cclib.io.ccopen(filename).parse()
+
+    # Check if the Gaussian terminate nomally
+    if not data.metadata["success"]:
+        print("Error termination! Gaussian did not end normally.")
+        return None
+
+    # Check imaginary frequency
+    try:
+        if data.vibfreqs[0] < 0: # freq calculation may not perform
+            print("Imaginary frequency is presented")
+            if not allow_imag:
+                print("***********Imaginary frequency is not allowed for this computation (by default).***********")
+                return None
+    except:
+        pass
+
+    # Obtain XYZ block of the converged structure
+    atomlabel = [periodictable.elements[i].symbol for i in data.atomnos]
+    coords = ["   ".join(map("{:.7f}".format, row)) for row in data.atomcoords[-1]] # the last coords (converged)
+    atomNcoords = ["   ".join(row) for row in zip(atomlabel, coords)]
+    cartesian = "\n".join(atomNcoords)
+    XYZBlock = str(data.natom) + "\n\n" + cartesian
+
+    # Embed molecule
+    mol = Chem.MolFromXYZBlock(XYZBlock)
+    set_cc_props(mol, name, data.charge, data.mult)
+    print_mol_data(mol)
+    print("Successfully load the molecule.")
+
+    return mol        
+        
+def pre_optimize(mol:Chem.Mol, method:str = "MMFF94"):
+    if method == "MMFF94":
+        AllChem.MMFFOptimizeMolecule(mol)
+
+
 if __name__ == "__main__":        
     mol = Chem.MolFromSmiles("CCCCCCCO")
     set_cc_props(mol)
